14737a9452e951c04e44727071aa979684f4e434
Ruslan <ruslan.rusu@yahoo.com>
2015-11-15 22:03:31 -0500

    Pack Unpack

--- OpLogSpec.cs
+++ OpLogSpec.cs
@@ -1,3 +1,8 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
 using NUnit.Framework;
 
 namespace LightRail
@@ -6,15 +11,98 @@ namespace LightRail
     public class OpLogSpec
     {
         [Test]
+        public void OpWrite()
+        {
+            var msg = "this is the hello world";
+
+            var output = Pack(msg);
+            var result = Unpack(output);
+
+            Console.WriteLine(result);
+        }
+
+        private object Unpack(byte[] output)
+        {
+            using (var memStream = new MemoryStream(output))
+            using (var reader = new BinaryReader(memStream))
+            {
+                var length = reader.ReadInt32();
+                var delivered = reader.ReadByte();
+                var hash = reader.ReadBytes(16);
+
+                var payloadLength = length - hash.Length - 1;
+
+                var payload = reader.ReadBytes(payloadLength);
+
+                Console.WriteLine("Hash :" + hash.Length);
+                Console.WriteLine("Payload : " + payload.Length);
+
+                return FromStream(new MemoryStream(payload));
+            }
+        }
+
+        private byte[] Pack(string msg)
+        {
+            using (var memStream = new MemoryStream())
+            using (var writer = new BinaryWriter(memStream))
+            {
+                var payload = ToStream(msg);
+                var hash = GetHashFor(payload);
+
+                Console.WriteLine("Hash :" + hash.Length);
+                Console.WriteLine("Payload : " + payload.Length);
+
+                var length = 1 + payload.Length + hash.Length;
+
+                writer.Write(length);
+
+                writer.Write((byte) 0);
+                writer.Write(hash);
+
+                Console.WriteLine("Position : " + memStream.Position);
+
+                writer.Write(payload.GetBuffer(), 0, (int) payload.Length);
+
+                return memStream.GetBuffer();
+            }
+        }
+
+        [Test]
         public void NamingWithPaddingZeros()
         {
             Assert.That(NamingScheme("out.", 0),       Is.EqualTo("out.000000000000.sf"));
             Assert.That(NamingScheme("in.", 4000000),  Is.EqualTo("in.000004000000.sf"));
         }
 
+        public MemoryStream ToStream(object o)
+        {
+            MemoryStream stream = new MemoryStream();
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, o);
+            return stream;
+        }
+
+        public object FromStream(MemoryStream stream)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            stream.Seek(0, SeekOrigin.Begin);
+            return formatter.Deserialize(stream);
+        }
+
+        public static byte[] GetHashFor(Stream s)
+        {
+            using (var md5 = MD5.Create())
+                return md5.ComputeHash(s);
+        }
+
         private static string NamingScheme(string prefix, int position)
         {
             return string.Format("{0}{1}.sf", prefix, position.ToString("D12"));
         }
+    }
+
+    public class Op
+    {
+         
     }
 }
